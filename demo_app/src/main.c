f0713b88f3159b98050d41fe4e9d3939c1cd1bd7
zhenquan.qiu <gnsyxiang@163.com>
2022-03-10 18:31:38 +0800

    build: add autotools to manager project

---
+++ demo_app/src/main.c
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    main.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    10/03 2022 16:57
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        10/03 2022      create the file
+ * 
+ *     last modified: 10/03 2022 16:57
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
