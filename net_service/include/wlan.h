91da5d3feac7e5b17a55cf367d4ba1d8a02932ee
zhenquan.qiu <gnsyxiang@163.com>
2022-03-24 16:34:49 +0800

    feat: add eth, wlan and 4G files

---
+++ net_service/include/wlan.h
@@ -0,0 +1,32 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    wlan.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    24/03 2022 16:27
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        24/03 2022      create the file
+ * 
+ *     last modified: 24/03 2022 16:27
+ */
+#ifndef __APP_FW_NET_SERVICE_INCLUDE_WLAN_H_
+#define __APP_FW_NET_SERVICE_INCLUDE_WLAN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
