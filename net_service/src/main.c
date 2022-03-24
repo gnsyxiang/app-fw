b4bb60efbbac8f154ce84d9d1672ad05ed5bb5c8
zhenquan.qiu <gnsyxiang@163.com>
2022-03-24 16:16:39 +0800

    feat: add net_service

---
+++ net_service/src/main.c
@@ -0,0 +1,25 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    main.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    24/03 2022 16:18
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        24/03 2022      create the file
+ * 
+ *     last modified: 24/03 2022 16:18
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
